f8c277101d591fb47fb3238fc8aac441d014dcc4
SoheibBoudali <39391107+SoheibBoudali@users.noreply.github.com>
2018-11-02 15:33:04 +0100

    3. Création d'un triggers qui vérifie avant modification du code service dans la table infirmier que la nouvelle valeur existe réellement, sinon, il refuse l’opération.

--- TP5.sql
+++ TP5.sql
@@ -31,7 +31,24 @@ BEGIN
 END;
 delete from infirmier where num_inf=0;
 
-/*Creation d'un trigger qui affiche « un nouveau infirmier est affecté à un [Nom de service] » après chaque insertion d’un infirmier.*/
+/*MAJ*/
+CREATE OR REPLACE TRIGGER MAJ
+AFTER INSERT OR UPDATE OR DELETE ON INFIRMIER FOR EACH ROW
+BEGIN
+	IF(INSERTING) THEN		
+		dbms_output.put_line('un nouveau employé de type infirmier est ajouté');
+	END IF;
+	IF(UPDATING) THEN
+		dbms_output.put_line('un employé de type infirmier modifié');
+	END IF;
+	IF(DELETING) THEN
+		dbms_output.put_line('Infirmier supprimé');
+	END IF;
+END; 
+
+
+/* 2 Creation d'un trigger qui affiche « un nouveau infirmier est affecté à un [Nom de service] »
+ après chaque insertion d’un infirmier.*/
 CREATE OR REPLACE TRIGGER Affectation_inf
 AFTER INSERT
    ON infirmier
@@ -40,3 +57,21 @@ BEGIN
 	dbms_output.put_line('un nouveau infirmier est affecté a '||:new.code_service);
 END;
 insert into infirmier values(0,'CAR','JOUR',4780);
+
+/* 3 Création d'un triggers qui vérifie avant modification du code service 
+dans la table infirmier que la nouvelle valeur existe réellement, sinon, il refuse l’opération.*/
+/*ALTER TABLE INFIRMIER DISABLE CONSTRAINT FK_SERVICEINF;*/
+
+CREATE OR REPLACE TRIGGER update_Service
+BEFORE UPDATE OF code_service
+   	ON infirmier
+	FOR EACH ROW
+        DECLARE
+    ICS infirmier.code_service%type;
+BEGIN
+   	SELECT code_service into ICS FROM service WHERE code_service = :new.code_service ;
+   	EXCEPTION WHEN NO_DATA_FOUND THEN
+        raise_application_error(-20000,'ERREUR LE CODE SERVICE NEXISTE PAS DANS LA TABLE SERVICE'); 
+END;
+UPDATE infirmier 
+SET code_service='XXX' WHERE num_inf=0;
